ca7c28db1996e6037a9db16696ab65230e151165
Camillo Bruni <cbruni@chromium.org>
2023-08-02 14:09:09 +0000

    Implement BrowserVersion object
    This will simplify version comparison and calculations in the future.
    
    Change-Id: Ic8ed36308e3764326a39bf80338901d77f15fe76
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/4735931
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Patrick Thier <pthier@chromium.org>

--- crossbench/browsers/browser.py
+++ crossbench/browsers/browser.py
@@ -5,12 +5,14 @@
 from __future__ import annotations
 
 import abc
+import functools
 import logging
 import pathlib
 import re
 import shutil
 from typing import TYPE_CHECKING, Any, Iterable, Optional, Sequence, Set, Tuple
 
+from crossbench import compat
 from crossbench.flags import Flags
 from crossbench.platform import PLATFORM, Platform
 
@@ -29,6 +31,91 @@ if TYPE_CHECKING:
 BROWSERS_CACHE = pathlib.Path(__file__).parents[2] / ".browsers-cache"
 
 # =============================================================================
+
+
+class BrowserVersionChannel(compat.StrEnum):
+  LTS = "lts"
+  STABLE = "stable"
+  BETA = "beta"
+  ALPHA = "alpha"
+  PRE_ALPHA = "pre-alpha"
+
+
+@functools.total_ordering
+class BrowserVersion(abc.ABC):
+
+  _parts: Tuple[int, ...]
+  _channel: BrowserVersionChannel
+  _version_str: str
+
+  def __init__(self, version: str) -> None:
+    (self._parts, self._channel, self._version_str) = self._parse(version)
+    if len(self._parts) < 2:
+      raise ValueError("Invalid version format")
+    for part in self._parts:
+      if part < 0:
+        raise ValueError(
+            f"Version parts must be positive, but got {self._parts}")
+
+  @abc.abstractmethod
+  def _parse(
+      self,
+      version: str,
+  ) -> Tuple[Tuple[int, ...], BrowserVersionChannel, str]:
+    pass
+
+  @property
+  def major(self) -> int:
+    return self._parts[0]
+
+  @property
+  def minor(self) -> int:
+    return self._parts[1]
+
+  @property
+  def channel(self) -> BrowserVersionChannel:
+    return self._channel
+
+  @property
+  def is_lts(self) -> bool:
+    return self.channel == BrowserVersionChannel.LTS
+
+  @property
+  def is_stable(self) -> bool:
+    return self.channel == BrowserVersionChannel.STABLE
+
+  @property
+  def is_beta(self) -> bool:
+    return self.channel == BrowserVersionChannel.BETA
+
+  @property
+  def is_alpha(self) -> bool:
+    return self.channel == BrowserVersionChannel.ALPHA
+
+  @property
+  def is_pre_alpha(self) -> bool:
+    return self.channel == BrowserVersionChannel.PRE_ALPHA
+
+  @property
+  def channel_name(self) -> str:
+    return self._channel_name(self.channel)
+
+  @abc.abstractmethod
+  def _channel_name(self, channel: BrowserVersionChannel) -> str:
+    pass
+
+  def __str__(self) -> str:
+    return f"{self._version_str} {self.channel_name}"
+
+  def __eq__(self, other: Any):
+    if not isinstance(other, BrowserVersion):
+      return False
+    return str(self) == str(other)
+
+  def __lt__(self, other: Any) -> bool:
+    assert isinstance(
+        other, BrowserVersion), (f"Expected BrowserVersion, but got {other}.")
+    return self._parts < other._parts
 
 
 class Browser(abc.ABC):
--- crossbench/browsers/firefox/firefox.py
+++ crossbench/browsers/firefox/firefox.py
@@ -10,7 +10,8 @@ import tempfile
 from typing import TYPE_CHECKING, Optional, Tuple
 
 from crossbench import helper
-from crossbench.browsers.browser import Browser
+from crossbench.browsers.browser import (Browser, BrowserVersion,
+                                         BrowserVersionChannel)
 from crossbench.browsers.viewport import Viewport
 from crossbench.browsers.webdriver import WebDriverBrowser
 
@@ -19,6 +20,54 @@ if TYPE_CHECKING:
   from crossbench.flags import Flags
   from crossbench.platform import Platform
   from crossbench.runner import Run
+
+
+class FirefoxVersion(BrowserVersion):
+  _VERSION_RE = re.compile(r"[^\d]+ (?P<version>"
+                           r"(?P<parts>\d+\.\d+(?P<channel>[ab.])\d+)"
+                           r")(?P<channel_esr>esr)?")
+  _SPLIT_RE = re.compile(r'[ab.]')
+  _CHANNEL_LOOKUP = {
+      "esr": (BrowserVersionChannel.LTS, 3),
+      ".": (BrowserVersionChannel.STABLE, 2),
+      # IRL Firefox version numbers do not distinct beta from stable, so we
+      # remap Firefox Dev => beta.
+      "b": (BrowserVersionChannel.BETA, 1),
+      "a": (BrowserVersionChannel.ALPHA, 0),
+  }
+
+  def _parse(
+      self,
+      version: str,
+  ) -> Tuple[Tuple[int, ...], BrowserVersionChannel, str]:
+    matches = self._VERSION_RE.fullmatch(version.strip())
+    if not matches:
+      raise ValueError(f"Could not extract version number from '{version}'")
+    version_str = matches["version"]
+    version_parts = matches["parts"]
+    assert version_parts and version_str
+    if matches["channel_esr"] and matches["channel"] != ".":
+      raise ValueError(f"Invalid ESR version: {version}")
+    channel: str = (matches["channel_esr"] or matches["channel"] or
+                    "stable").lower()
+    browser_channel, channel_id = self._CHANNEL_LOOKUP[channel]
+    parts = tuple(map(int, self._SPLIT_RE.split(version_parts)))
+    if len(parts) != 3:
+      raise ValueError(f"Invalid number of version number parts in '{version}'")
+    # Inject browser_channel into the version parts to make it unique
+    parts = parts[:-1] + (channel_id, parts[-1])
+    return parts, browser_channel, version_str
+
+  def _channel_name(self, channel: BrowserVersionChannel) -> str:
+    if channel == BrowserVersionChannel.LTS:
+      return "esr"
+    if channel == BrowserVersionChannel.STABLE:
+      return "stable"
+    if channel == BrowserVersionChannel.BETA:
+      return "dev"
+    if channel == BrowserVersionChannel.ALPHA:
+      return "nightly"
+    raise ValueError(f"Unsupported channel: {channel}")
 
 
 class Firefox(Browser):
