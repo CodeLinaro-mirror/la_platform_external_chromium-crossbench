165b6566767e9f30b9264ea844c3e33305b8432e
Max Li <maxqli@google.com>
2023-11-16 16:43:08 +0000

    Fix copybara error.
    - Added init file to enable presubmit check in analysis folder, so now the copybara error will be caught in gerrit presubmit.
    - Use full path import to avoid presubmit check error.
    - Minor format update via `git cl format`
    
    Change-Id: Ia8ff6a53fddabd1aff88472579826bb8b765080b
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/5035296
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Commit-Queue: Max Li <maxqli@google.com>

--- crossbench/benchmarks/experimental/power/analysis/power_result_parser.py
+++ crossbench/benchmarks/experimental/power/analysis/power_result_parser.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from __future__ import annotations
+
 import argparse
 import json
 import statistics
@@ -15,7 +17,7 @@ MAX_CAPACITY = "battery_max_capacity"
 VOLTAGE = "battery_voltage"
 
 def avg_power(power_samples: List[Dict[str, float]]):
-    """Calculates average power consumption.
+  """Calculates average power consumption.
 
     Args:
         power_samples: List of samples of power usage. Here is an example:
@@ -39,34 +41,39 @@ def avg_power(power_samples: List[Dict[str, float]]):
     Returns:
         Average power consumption in watts.
     """
-    start_sample = power_samples[0]
-    end_sample = power_samples[-1]
-    start_consumed_ah = start_sample[MAX_CAPACITY] - start_sample[CURRENT_CAPACITY]
-    end_consumed_ah = end_sample[MAX_CAPACITY] - end_sample[CURRENT_CAPACITY]
-    delta_consumed_ah = start_consumed_ah - end_consumed_ah
-    avg_voltage_v = statistics.fmean(sample[VOLTAGE] for sample in power_samples)
-    duration_sec = (end_sample[SAMPLE_TIME] - start_sample[SAMPLE_TIME]) / 10**6
-    avg_current_a = delta_consumed_ah * 3600.0 / duration_sec
-    return avg_voltage_v * -avg_current_a
+  start_sample = power_samples[0]
+  end_sample = power_samples[-1]
+  start_consumed_ah = start_sample[MAX_CAPACITY] - start_sample[CURRENT_CAPACITY]
+  end_consumed_ah = end_sample[MAX_CAPACITY] - end_sample[CURRENT_CAPACITY]
+  delta_consumed_ah = start_consumed_ah - end_consumed_ah
+  avg_voltage_v = statistics.fmean(sample[VOLTAGE] for sample in power_samples)
+  duration_sec = (end_sample[SAMPLE_TIME] - start_sample[SAMPLE_TIME]) / 10**6
+  avg_current_a = delta_consumed_ah * 3600.0 / duration_sec
+  return avg_voltage_v * -avg_current_a
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("battery_result_file", 
-                        help="`powersampler.power_battery.json` file path.")
-    args = parser.parse_args()
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      "battery_result_file",
+      help="`powersampler.power_battery.json` file path.")
+  args = parser.parse_args()
 
-    with open(args.battery_result_file) as f:
-        result_json = json.load(f)
-        power_samples = result_json["data_rows"]
+  with open(args.battery_result_file) as f:
+    result_json = json.load(f)
+    power_samples = result_json["data_rows"]
 
-        if len(power_samples) < 2:
-            print("At least two power samples are needed to calculate power consumption.")
-            exit(1)
+    if len(power_samples) < 2:
+      print(
+          "At least two power samples are needed to calculate power consumption."
+      )
+      exit(1)
 
-        if any(sample[EXTERNAL_CONNECTED] for sample in power_samples):
-            print("External power was connected during the benchmark,",
-                  "please only use battery during benchmarking to get valid results.")
-            exit(1)
+    if any(sample[EXTERNAL_CONNECTED] for sample in power_samples):
+      print(
+          "External power was connected during the benchmark,",
+          "please only use battery during benchmarking to get valid results.")
+      exit(1)
 
-        print("Average power consumption (W): {:.3f}".format(avg_power(power_samples)))
+    print("Average power consumption (W): {:.3f}".format(
+        avg_power(power_samples)))
