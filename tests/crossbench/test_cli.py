ed64dda97d6162def99456fa5ff60bdb1804e2b7
Camillo Bruni <cbruni@chromium.org>
2024-07-09 10:37:25 +0000

    Move slower presubmit tests to separate files
    Hopefully this will speed up uploading CLs a bit in the future.
    
    Change-Id: I14a5afdc80a94ca7a47bc6a83baa0435e38aa36c
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/5686236
    Reviewed-by: Patrick Thier <pthier@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>

--- tests/crossbench/test_cli.py
+++ tests/crossbench/test_cli.py
@@ -1,4 +1,4 @@
-# Copyright 2022 The Chromium Authors
+# Copyright 2024 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -7,7 +7,7 @@ import datetime as dt
 import json
 import pathlib
 import unittest
-from typing import Dict, List, Optional, Type
+from typing import List, Optional, Type
 from unittest import mock
 
 import hjson
@@ -18,8 +18,6 @@ from crossbench.cli.cli import CrossBenchCLI
 from crossbench.cli.config import BrowserConfig, BrowserVariantsConfig
 from crossbench.cli.config.driver import BrowserDriverType, DriverConfig
 from crossbench.env import HostEnvironmentConfig, ValidationMode
-from crossbench.network.base import Network
-from crossbench.network.local_fileserver import LocalFileNetwork
 from crossbench.path import RemotePath
 from crossbench.probes import internal
 from crossbench.runner.runner import Runner
@@ -29,7 +27,9 @@ from tests.crossbench.mock_helper import BaseCliTestCase, SysExitTestException
 from tests.crossbench.test_cli_config import XCTRACE_DEVICES_SINGLE_OUTPUT
 
 
-class CliTestCase(BaseCliTestCase):
+class FastCliTestCase(BaseCliTestCase):
+  """These tests are run as part of the presubmit and should be
+  reasonably fast. Slow tests run on the CQ are in CliSlowTestCase."""
 
   def test_invalid(self):
     with self.assertRaises(SysExitTestException):
@@ -170,42 +170,6 @@ class CliTestCase(BaseCliTestCase):
     self.assertIn("--no-color", stdout)
     self.assertIn("Disable colored output", stdout)
     self.assertIn("Available Probes for all Benchmarks:", stdout)
-
-  def test_subcommand_help(self):
-    for benchmark_cls in CrossBenchCLI.BENCHMARKS:
-      subcommands = (benchmark_cls.NAME,) + benchmark_cls.aliases()
-      for subcommand in subcommands:
-        with self.assertRaises(SysExitTestException) as cm:
-          self.run_cli(subcommand, "--help")
-        self.assertEqual(cm.exception.exit_code, 0)
-        _, stdout, stderr = self.run_cli_output(
-            subcommand, "--help", raises=SysExitTestException)
-        self.assertFalse(stderr)
-        self.assertIn("--env-validation ENV_VALIDATION", stdout)
-
-  def test_subcommand_help_subcommand(self):
-    for benchmark_cls in CrossBenchCLI.BENCHMARKS:
-      subcommands = (benchmark_cls.NAME,) + benchmark_cls.aliases()
-      for subcommand in subcommands:
-        with self.assertRaises(SysExitTestException) as cm:
-          self.run_cli(subcommand, "help")
-        self.assertEqual(cm.exception.exit_code, 0)
-        _, stdout, stderr = self.run_cli_output(
-            subcommand, "help", raises=SysExitTestException)
-        self.assertFalse(stderr)
-        self.assertIn("--env-validation ENV_VALIDATION", stdout)
-
-  def test_subcommand_describe_subcommand(self):
-    for benchmark_cls in CrossBenchCLI.BENCHMARKS:
-      subcommands = (benchmark_cls.NAME,) + benchmark_cls.aliases()
-      for subcommand in subcommands:
-        with self.assertRaises(SysExitTestException) as cm:
-          self.run_cli(subcommand, "describe")
-        self.assertEqual(cm.exception.exit_code, 0)
-        _, stdout, stderr = self.run_cli_output(
-            subcommand, "describe", raises=SysExitTestException)
-        output = stderr + stdout
-        self.assertIn("See `describe benchmark ", output)
 
   def test_version(self):
     with self.assertRaises(SysExitTestException) as cm:
@@ -354,42 +318,6 @@ class CliTestCase(BaseCliTestCase):
       message = str(cm.exception)
       self.assertIn("--config", message)
       self.assertIn(config_flag, message)
-
-  def test_config_file_with_network(self):
-    local_server_path = pathlib.Path("custom/server")
-    local_server_path.mkdir(parents=True)
-    self.fs.create_file(local_server_path / "index.html", st_size=100)
-    config_file = pathlib.Path("/config.hjson")
-    config_data = {
-        "probes": {},
-        "env": {},
-        "browsers": {},
-        "network": str(local_server_path),
-    }
-    with config_file.open("w", encoding="utf-8") as f:
-      hjson.dump(config_data, f)
-
-    browsers = []
-
-    def get_browser(self, args: argparse.Namespace):
-      browsers = [
-          mock_browser.MockChromeDev(
-              "dev",
-              platform=self.platform,
-              network=args.network.create(self.platform)),
-      ]
-      return browsers
-
-    with mock.patch.object(CrossBenchCLI, "_get_browsers", get_browser):
-      url = "http://test.com"
-      self.run_cli("loading", f"--config={config_file}", f"--urls={url}",
-                   "--env-validation=skip")
-      for browser in browsers:
-        self.assertListEqual([url], browser.url_list[self.SPLASH_URLS_LEN:])
-        assert isinstance(browser.network, LocalFileNetwork)
-        network: LocalFileNetwork = browser.network
-        self.assertFalse(network.is_live)
-        self.assertEqual(network.path, local_server_path)
 
   def test_config_file_with_probe(self):
     config_file = pathlib.Path("/config.hjson")
@@ -552,58 +480,6 @@ class CliTestCase(BaseCliTestCase):
     self.assertListEqual(["--chrome-flag1=value1", "--chrome-flag2"],
                          list(browser.flags))
 
-  def test_browser_identifiers(self):
-    browsers: Dict[str, Type[mock_browser.MockBrowser]] = {
-        "chrome": mock_browser.MockChromeStable,
-        "chrome-stable": mock_browser.MockChromeStable,
-        "chr-stable": mock_browser.MockChromeStable,
-        "chrome-beta": mock_browser.MockChromeBeta,
-        "chr-beta": mock_browser.MockChromeBeta,
-        "chrome-dev": mock_browser.MockChromeDev,
-        "edge": mock_browser.MockEdgeStable,
-        "edge-stable": mock_browser.MockEdgeStable,
-        "edge-beta": mock_browser.MockEdgeBeta,
-        "edge-dev": mock_browser.MockEdgeDev,
-        "ff": mock_browser.MockFirefox,
-        "firefox": mock_browser.MockFirefox,
-        "firefox-dev": mock_browser.MockFirefoxDeveloperEdition,
-        "firefox-developer-edition": mock_browser.MockFirefoxDeveloperEdition,
-        "ff-dev": mock_browser.MockFirefoxDeveloperEdition,
-        "firefox-nightly": mock_browser.MockFirefoxNightly,
-        "ff-nightly": mock_browser.MockFirefoxNightly,
-        "ff-trunk": mock_browser.MockFirefoxNightly,
-    }
-    if not self.platform.is_linux:
-      browsers["chr-canary"] = mock_browser.MockChromeCanary
-      browsers["chrome-canary"] = mock_browser.MockChromeCanary
-      browsers["edge-canary"] = mock_browser.MockEdgeCanary
-    if self.platform.is_macos:
-      browsers.update({
-          "safari": mock_browser.MockSafari,
-          "sf": mock_browser.MockSafari,
-          "safari-technology-preview": mock_browser.MockSafariTechnologyPreview,
-          "sf-tp": mock_browser.MockSafariTechnologyPreview,
-          "tp": mock_browser.MockSafariTechnologyPreview,
-      })
-
-    for identifier, browser_cls in browsers.items():
-      out_dir = self.out_dir / identifier
-      self.assertFalse(out_dir.exists())
-      with mock.patch.object(
-          BrowserVariantsConfig, "_get_browser_cls",
-          return_value=browser_cls) as get_browser_cls:
-        url = "http://test.com"
-        self.run_cli("loading", f"--browser={identifier}", f"--urls={url}",
-                     "--env-validation=skip", f"--out-dir={out_dir}")
-        self.assertTrue(out_dir.exists())
-        get_browser_cls.assert_called_once()
-        result_files = list(
-            out_dir.glob(f"**/{internal.ResultsSummaryProbe.NAME}.json"))
-        result_file = result_files[1]
-        with result_file.open(encoding="utf-8") as f:
-          results = json.load(f)
-        self.assertEqual(results["browser"]["version"], browser_cls.VERSION)
-        self.assertIn("test.com", results["stories"])
 
   def test_browser_identifiers_duplicate(self):
     with self.assertRaises(argparse.ArgumentTypeError):
@@ -877,52 +753,7 @@ class CliTestCase(BaseCliTestCase):
     with self.assertRaises(SysExitTestException):
       self.run_cli("loading", f"--env-config={config}",
                    "--urls=http://test.com", "--env-validation=skip")
-
-  def test_multiple_browser_compatible_flags(self):
-    mock_browsers: List[Type[mock_browser.MockBrowser]] = [
-        mock_browser.MockChromeStable,
-        mock_browser.MockFirefox,
-        mock_browser.MockChromeDev,
-    ]
-
-    def mock_get_browser_cls(browser_config: BrowserConfig):
-      self.assertEqual(browser_config.driver.type, BrowserDriverType.WEB_DRIVER)
-      for mock_browser_cls in mock_browsers:
-        if mock_browser_cls.mock_app_path() == browser_config.path:
-          return mock_browser_cls
-      raise ValueError("Unknown browser path")
-
-    for chrome_flag in ("--js-flags=--no-opt", "--enable-features=Foo",
-                        "--disable-features=bar"):
-      # Fail for chrome flags for non-chrome browser
-      with self.assertRaises(argparse.ArgumentTypeError), mock.patch.object(
-          BrowserVariantsConfig,
-          "_get_browser_cls",
-          side_effect=mock_get_browser_cls):
-        self.run_cli("loading", "--urls=http://test.com",
-                     "--env-validation=skip", "--throw", "--browser=firefox",
-                     chrome_flag)
-      # Fail for mixed browsers and chrome flags
-      with self.assertRaises(argparse.ArgumentTypeError), mock.patch.object(
-          BrowserVariantsConfig,
-          "_get_browser_cls",
-          side_effect=mock_get_browser_cls):
-        self.run_cli("loading", "--urls=http://test.com",
-                     "--env-validation=skip", "--throw", "--browser=chrome",
-                     "--browser=firefox", chrome_flag)
-      with self.assertRaises(argparse.ArgumentTypeError), mock.patch.object(
-          BrowserVariantsConfig,
-          "_get_browser_cls",
-          side_effect=mock_get_browser_cls):
-        self.run_cli("loading", "--urls=http://test.com",
-                     "--env-validation=skip", "--throw", "--browser=chrome",
-                     "--browser=firefox", "--", chrome_flag)
-    # Flags for the same type are allowed.
-    with self.patch_get_browser():
-      self.run_cli("loading", "--urls=http://test.com", "--env-validation=skip",
-                   "--throw", "--browser=chrome", "--browser=chrome-dev", "--",
-                   "--js-flags=--no-opt")
-
+ 
   def test_parse_env_config_file(self):
     config = pathlib.Path("/test.config.hjson")
     with config.open("w", encoding="utf-8") as f:
