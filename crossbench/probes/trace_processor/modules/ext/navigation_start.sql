3d7537a19a0ee04b02883099093693d5ed0fa76f
Igor Kraskevich <kraskevich@google.com>
2024-08-30 14:52:59 +0000

    Add desktop CNN page load metric
    The sequence of actions is:
    - Load the website
    - Click on the cookie banner with injected JS
    - Cookie consent triggers a complete page reload
    - Measure the page reload duration, ignoring the first load
    
    Change-Id: I47719050c9bafd7dcf50e30bdbda2ca2760f0e2a
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/5828862
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Commit-Queue: Igor Kraskevich <kraskevich@google.com>

--- crossbench/probes/trace_processor/modules/ext/navigation_start.sql
+++ crossbench/probes/trace_processor/modules/ext/navigation_start.sql
@@ -4,3 +4,11 @@ CREATE VIEW navigation_start AS
 SELECT MIN(ts) AS navigation_start
 FROM slice
 WHERE name = 'PageLoadMetrics.NavigationToLargestContentfulPaint';
+
+
+DROP VIEW IF EXISTS last_navigation_start;
+
+CREATE VIEW last_navigation_start AS
+SELECT MAX(ts) AS last_navigation_start
+FROM slice
+WHERE name = 'PageLoadMetrics.NavigationToLargestContentfulPaint';
