54949bf3a724dbef77e16dade56c64b428b02e6c
Camillo Bruni <cbruni@chromium.org>
2024-11-14 10:25:14 +0000

    Improve end2end test setup
    - Move test_cli.py to desktop
    - Add helper runners to desktop and android to only run those tests
    
    Bug: 378896416
    Change-Id: Iafba1aae11fec3bfdaab2812cff7fbea417b8bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/6023156
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>

---
+++ tests/end2end/android/runner.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env vpython3
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from __future__ import annotations
+
+import pathlib
+import sys
+
+import pytest
+
+FILE_PATH = pathlib.Path(__file__).absolute()
+TEST_DIR = FILE_PATH.absolute().parent
+REPO_DIR = FILE_PATH.absolute().parents[3]
+
+if REPO_DIR not in sys.path:
+  sys.path.insert(0, str(REPO_DIR))
+
+if __name__ == "__main__":
+  pass_through_args = sys.argv[1:]
+  return_code = pytest.main([
+      "--exitfirst", "--verbose", "--dist=loadgroup", "--log-cli-level=DEBUG",
+      "-o", "log_cli=True", "-rs",
+      str(TEST_DIR), *pass_through_args
+  ])
+  sys.exit(return_code)
