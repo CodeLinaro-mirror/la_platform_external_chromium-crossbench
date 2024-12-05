4e02394871804ff87a626aa7c02eb3ddec7aca73
Kameron Lutes <kalutes@google.com>
2024-12-05 02:19:16 +0000

    Add end2end test for android scrolling
    Change-Id: Ieb5f8a1ccf7e12c1ff6fbe0fb8748944049bc42e
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/6064624
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Commit-Queue: Kameron Lutes <kalutes@chromium.org>

--- tests/end2end/android/test_loading.py
+++ tests/end2end/android/test_loading.py
@@ -14,6 +14,18 @@ from crossbench.benchmarks.loading.input_source import InputSource
 from crossbench.cli.cli import CrossBenchCLI
 from tests import test_helper
 
+def _run_loading_test(browser_config, page_config) -> None:
+  with tempfile.NamedTemporaryFile() as page_config_file:
+    with open(page_config_file.name, mode="w", encoding="utf-8") as f:
+      json.dump(page_config, f)
+
+    cli = CrossBenchCLI()
+
+    cli.run([
+        "loading", f"--browser={browser_config}",
+        f"--page-config={page_config_file.name}", "--action-runner=android"
+    ])
+
 
 @pytest.mark.parametrize("input_source", InputSource)
 def test_click(browser_config, input_source) -> None:
@@ -69,16 +81,74 @@ def test_click(browser_config, input_source) -> None:
       }
   }
 
-  with tempfile.NamedTemporaryFile() as page_config_file:
-    with open(page_config_file.name, mode="w", encoding="utf-8") as f:
-      json.dump(page_config, f)
+  _run_loading_test(browser_config, page_config)
 
-    cli = CrossBenchCLI()
 
-    cli.run([
-        "loading", f"--browser={browser_config}",
-        f"--page-config={page_config_file.name}", "--action-runner=android"
-    ])
+def test_scroll(browser_config) -> None:
+
+  test_page = urllib.parse.quote("""
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Scroll Test</title>
+  <style>
+    #scrollable-area {
+      height: 200px;
+      overflow-y: auto;
+    }
+    #content {
+      height: 500px;
+    }
+  </style>
+</head>
+<body>
+  <div id="no-scroll"></div>
+  <div id="scrollable-area">
+    <div id="content">
+    </div>
+  </div>
+  <script>
+    const scrollableArea = document.getElementById('scrollable-area');
+    scrollableArea.addEventListener('scroll', function() {
+      document.getElementById('no-scroll').id = 'yes-scroll';
+    });
+  </script>
+</body>
+</html>
+""")
+
+  page_config = {
+      "pages": {
+          "ClickTest": {
+              "actions": [
+                  {
+                      "action": "get",
+                      "url": f"data:text/html;charset=utf-8,{test_page}",
+                      "ready_state": "complete"
+                  },
+                  {
+                      "action": "wait_for_element",
+                      "selector": "div[id='scrollable-area']",
+                      "timeout": "10s"
+                  },
+                  {
+                      "action": "scroll",
+                      "selector": "div[id='scrollable-area']",
+                      "required": True,
+                      "source": "touch",
+                      "distance": 50,
+                  },
+                  {
+                      "action": "wait_for_element",
+                      "selector": "div[id='yes-scroll']",
+                      "timeout": "1s"
+                  },
+              ]
+          }
+      }
+  }
+
+  _run_loading_test(browser_config, page_config)
 
 
 if __name__ == "__main__":
