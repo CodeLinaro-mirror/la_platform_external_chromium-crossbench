3ffc0b58021fc8962a361b79f27d92934efd5120
Camillo Bruni <cbruni@chromium.org>
2025-04-29 00:55:38 -0700

    Force python utf8 encoding
    Windows sometimes uses legacy encodings which causes issues with
    our very-much-needed emoji output.
    
    Bug: 406830023
    Change-Id: I412987db5f43d4e94e767357132a6a716d29ce26
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/6494933
    Reviewed-by: Patrick Thier <pthier@chromium.org>
    Commit-Queue: Patrick Thier <pthier@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Auto-Submit: Camillo Bruni <cbruni@chromium.org>

--- cb_btp.py
+++ cb_btp.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env vpython3
+#!/usr/bin/env vpython3 -Xutf8
 # Copyright 2024 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 import sys
+
 from crossbench.cli.btp import BTPUtil
 
 if __name__ == "__main__":
