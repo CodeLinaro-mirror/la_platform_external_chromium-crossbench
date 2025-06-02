3884f87267838a31ea6254fe2d427df6396ce7c2
Camillo Bruni <cbruni@chromium.org>
2025-06-02 03:45:34 -0700

    Split flag files
    Drive-by-fix:
    - Rename ChromeFlags._JS_FLAG to JS_FLAG and mark it Final
    
    Change-Id: I87c3a9b6e79ae07bac954dc3b7b4e7a9a20a533d
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/6606043
    Reviewed-by: Patrick Thier <pthier@chromium.org>
    Auto-Submit: Camillo Bruni <cbruni@chromium.org>
    Commit-Queue: Patrick Thier <pthier@chromium.org>

--- crossbench/flags/chrome.py
+++ crossbench/flags/chrome.py
@@ -4,16 +4,16 @@
 
 from __future__ import annotations
 
-import abc
 import logging
-from typing import Dict, Final, Iterable, Iterator, Optional, Tuple
+from typing import Final, Iterable, Optional, Tuple
 
-from ordered_set import OrderedSet
 from typing_extensions import override
 
 from crossbench import path as pth
-from crossbench.flags.base import Flags, FlagsData, Freezable
+from crossbench.flags.base import Flags, FlagsData
 from crossbench.flags.chrome_extensions import ChromeExtensions
+from crossbench.flags.chrome_features import (ChromeBlinkFeatures,
+                                              ChromeFeatures)
 from crossbench.flags.js_flags import JSFlags
 from crossbench.flags.known_js_flags import KNOWN_JS_FLAGS
 
@@ -25,7 +25,7 @@ class ChromeFlags(Flags):
   --enable-features/--disable-features
   --enable-blink-features/--disable-blink-features
   """
-  _JS_FLAG = "--js-flags"
+  JS_FLAG: Final[str] = "--js-flags"
 
   # All flags that might affect how finch / field-trials are loaded.
   FIELD_TRIAL_FLAGS: Final[Tuple[str, ...]] = (
@@ -68,7 +68,7 @@ class ChromeFlags(Flags):
     return self
 
   def __getitem__(self, key):
-    if key == self._JS_FLAG and self._js_flags:
+    if key == self.JS_FLAG and self._js_flags:
       return self._js_flags
     if key == ChromeFeatures.ENABLE_FLAG and self._features.enabled:
       return self._features.enabled_str()
@@ -133,7 +133,7 @@ class ChromeFlags(Flags):
         for feature in flag_value.split(","):
           self._blink_features.disable(feature)
       return True
-    if flag_name == self._JS_FLAG:
+    if flag_name == self.JS_FLAG:
       if flag_value is None:
         self._js_flags.clear()
       else:
@@ -263,7 +263,7 @@ class ChromeFlags(Flags):
   def items(self) -> Iterable[Tuple[str, str | None]]:  # type: ignore
     yield from self.base_items()
     if self._js_flags:
-      yield (self._JS_FLAG, str(self.js_flags))
+      yield (self.JS_FLAG, str(self.js_flags))
     yield from self.features.items()
     yield from self.blink_features.items()
     yield from self.extensions.items()
@@ -271,157 +271,3 @@ class ChromeFlags(Flags):
   def __bool__(self) -> bool:
     return bool(self.data) or bool(self._js_flags) or bool(
         self._features) or bool(self._blink_features) or bool(self._extensions)
-
-
-class ChromeBaseFeatures(Freezable, abc.ABC):
-  ENABLE_FLAG: str = ""
-  DISABLE_FLAG: str = ""
-
-  def __init__(self) -> None:
-    super().__init__()
-    self._enabled: Dict[str, str | None] = {}
-    self._disabled: OrderedSet[str] = OrderedSet()
-
-  @property
-  def is_empty(self) -> bool:
-    return len(self._enabled) == 0 and len(self._disabled) == 0
-
-  @property
-  def enabled(self) -> Dict[str, Optional[str]]:
-    return dict(self._enabled)
-
-  @property
-  def disabled(self) -> OrderedSet[str]:
-    return OrderedSet(self._disabled)
-
-  def _parse_feature(self, feature: str) -> Tuple[str, Optional[str]]:
-    if not feature:
-      raise ValueError("Cannot parse empty feature")
-    if "," in feature:
-      raise ValueError(f"{repr(feature)} contains multiple features. "
-                       "Please split them first.")
-    return self._parse_feature_parts(feature)
-
-  @abc.abstractmethod
-  def _parse_feature_parts(self, feature: str) -> Tuple[str, Optional[str]]:
-    pass
-
-  def enable(self, feature: str) -> None:
-    name, value = self._parse_feature(feature)
-    self._enable(name, value)
-
-  def _enable(self, name: str, value: Optional[str]) -> None:
-    self.assert_not_frozen()
-    if name in self._disabled:
-      raise ValueError(
-          f"Cannot enable previously disabled feature={repr(name)}")
-    if name in self._enabled:
-      prev_value = self._enabled[name]
-      if value != prev_value:
-        raise ValueError("Cannot set conflicting values "
-                         f"({repr(prev_value)}, vs. {repr(value)}) "
-                         f"for the same feature={repr(name)}")
-    else:
-      self._enabled[name] = value
-
-  def disable(self, feature: str) -> None:
-    self.assert_not_frozen()
-    name, _ = self._parse_feature(feature)
-    if name in self._enabled:
-      raise ValueError(
-          f"Cannot disable previously enabled feature={repr(name)}")
-    self._disabled.add(name)
-
-  def clear_enabled(self):
-    self.assert_not_frozen()
-    self._enabled = {}
-
-  def clear_disabled(self):
-    self.assert_not_frozen()
-    self._disabled = OrderedSet()
-
-  def update(self, other: ChromeBaseFeatures) -> None:
-    if not isinstance(other, type(self)):
-      raise TypeError(f"Cannot merge {type(self)} with {type(other)}")
-    for disabled in other.disabled:
-      self.disable(disabled)
-    for name, value in other.enabled.items():
-      self._enable(name, value)
-
-  def merge(self, other: ChromeBaseFeatures) -> None:
-    self.update(other)
-
-  def items(self) -> Iterable[Tuple[str, str]]:
-    if self._enabled:
-      yield (self.ENABLE_FLAG, self.enabled_str())
-    if self._disabled:
-      yield (self.DISABLE_FLAG, self.disabled_str())
-
-  def enabled_str(self) -> str:
-    return ",".join(
-        k if v is None else f"{k}{v}" for k, v in self._enabled.items())
-
-  def disabled_str(self) -> str:
-    return ",".join(self._disabled)
-
-  def __iter__(self) -> Iterator[str]:
-    for flag_name, features_str in self.items():
-      yield f"{flag_name}={features_str}"
-
-  def __bool__(self) -> bool:
-    return bool(self._enabled) or bool(self._disabled)
-
-  def __str__(self) -> str:
-    return " ".join(self)
-
-  def __contains__(self, feature: str) -> bool:
-    return feature in self._disabled or feature in self._enabled
-
-
-class ChromeFeatures(ChromeBaseFeatures):
-  """
-  Chrome Features set, throws if features are enabled and disabled at the same
-  time.
-  Examples:
-    --disable-features="MyFeature1"
-    --enable-features="MyFeature1,MyFeature2"
-    --enable-features="MyFeature1:k1/v1/k2/v2,MyFeature2"
-    --enable-features="MyFeature3<Trial2:k1/v1/k2/v2"
-  """
-
-  ENABLE_FLAG: str = "--enable-features"
-  DISABLE_FLAG: str = "--disable-features"
-
-  @override
-  def _parse_feature_parts(self, feature: str) -> Tuple[str, Optional[str]]:
-    parts = feature.split("<")
-    if len(parts) == 2:
-      return (parts[0], "<" + parts[1])
-    if len(parts) != 1:
-      raise ValueError(f"Invalid number of feature parts: {repr(parts)}")
-    parts = feature.split(":")
-    if len(parts) == 2:
-      return (parts[0], ":" + parts[1])
-    if len(parts) != 1:
-      raise ValueError(f"Invalid number of feature parts: {repr(parts)}")
-    return (feature, None)
-
-
-class ChromeBlinkFeatures(ChromeBaseFeatures):
-  """
-  Chrome Features set, throws if features are enabled and disabled at the same
-  time.
-  Examples:
-    --disable-blink-features="MyFeature1"
-    --enable-blink-features="MyFeature1,MyFeature2"
-  """
-
-  ENABLE_FLAG: str = "--enable-blink-features"
-  DISABLE_FLAG: str = "--disable-blink-features"
-
-  @override
-  def _parse_feature_parts(self, feature: str) -> Tuple[str, Optional[str]]:
-    if "<" in feature or ":" in feature:
-      raise ValueError("blink features do not have params, "
-                       f"but found param separator in {repr(feature)}")
-    return (feature, None)
