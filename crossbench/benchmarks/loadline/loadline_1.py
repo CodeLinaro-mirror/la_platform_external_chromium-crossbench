e5f52e392ef8fae9e4fd83e2d6af366999f28776
Mikhail Khokhlov <khokhlov@google.com>
2025-06-12 09:12:29 -0700

    [LoadLine] Switch to archives with pre-injected deterministic.js
    Injecting a script on-the-go can be slow and this can become a blocker
    in the page loading process. To make sure LoadLine correctly measures
    the browser page loading performance, we disable runtime script
    injection and use an archive with pre-injected script instead.
    
    Bump the LoadLine version since on some devices this can affect scores.
    
    Change-Id: I9a1079f741ad32351aab9f7f5d98904aa346e835
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/6634600
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>

--- crossbench/benchmarks/loadline/loadline_1.py
+++ crossbench/benchmarks/loadline/loadline_1.py
@@ -26,7 +26,7 @@ if TYPE_CHECKING:
 
 # We should increase the minor version number every time there are any changes
 # that might affect the benchmark score.
-VERSION_STRING = "1.2.0"
+VERSION_STRING = "1.3.0"
 
 
 class LoadLine1Probe(LoadLineProbe):
