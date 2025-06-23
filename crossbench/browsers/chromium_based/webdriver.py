749d1a4f48bb0ae63aa8559ef96180a677b65307
Charles William Dick <cwd@google.com>
2025-06-23 00:07:44 -0700

    loading switch_tab search instead of match
    So we can write "example.com" and have it match http://example.com.
    
    Change-Id: I4952d79eb0ae37adbdd4c49593847446a8c324c6
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/6658463
    Commit-Queue: Charles Dick <cwd@google.com>
    Reviewed-by: Kameron Lutes <kalutes@google.com>

--- crossbench/browsers/chromium_based/webdriver.py
+++ crossbench/browsers/chromium_based/webdriver.py
@@ -256,10 +256,10 @@ class ChromiumBasedWebDriver(
       for handle in handles:
         driver.switch_to.window(handle)
         if title is not None:
-          if title.match(driver.title) is None:
+          if title.search(driver.title) is None:
             continue
         if url is not None:
-          if url.match(driver.current_url) is None:
+          if url.search(driver.current_url) is None:
             continue
         return handle
     error = "No new tab found"
