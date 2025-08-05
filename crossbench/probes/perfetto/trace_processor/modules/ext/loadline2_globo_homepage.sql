9cefed1fd50cd4e4d8953cc74584963bb1300f5c
Mikhail Khokhlov <khokhlov@google.com>
2025-08-05 09:50:39 -0700

    [LoadLine2] Implement visual+interactive metrics
    The new set of metrics computes two values for each page: one
    representing "visual" completeness (an important element appears in
    DOM), the other "interactive" completeness (the page can be interacted
    with). This better reflects different aspects of the page loading
    process and allows us to reduce measurement noise.
    
    Also reduce the default number of repetitions to 50, to improve
    running time and make thermal issues less likely.
    
    Change-Id: I0df74987f74cdb56fded4d4969ca10a005cc5110
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/6797372
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Reviewed-by: Victor Vianna <victorvianna@google.com>

--- crossbench/probes/perfetto/trace_processor/modules/ext/loadline2_globo_homepage.sql
+++ crossbench/probes/perfetto/trace_processor/modules/ext/loadline2_globo_homepage.sql
@@ -1,6 +1,15 @@
 INCLUDE PERFETTO MODULE ext.first_presentation_time;
 
-CREATE OR REPLACE PERFETTO FUNCTION loadline2_globo_homepage_score()
+CREATE OR REPLACE PERFETTO FUNCTION loadline2_globo_homepage_score_visual()
+RETURNS FLOAT
+AS
+SELECT
+  -- Multiply by 60 to make the score per minutes rather than per second.
+  60e9 / (
+    get_presentation_time('LoadLine2/globo_homepage/headline_shown')
+    - get_event_time('LoadLine2/phone/globo_homepage_start'));
+
+CREATE OR REPLACE PERFETTO FUNCTION loadline2_globo_homepage_score_interactive()
 RETURNS FLOAT
 AS
 SELECT
--- crossbench/probes/perfetto/trace_processor/queries/loadline2/benchmark_score_tablet.sql
+++ crossbench/probes/perfetto/trace_processor/queries/loadline2/benchmark_score_tablet.sql
@@ -5,9 +5,13 @@ INCLUDE PERFETTO MODULE ext.loadline2_google_search_result;
 INCLUDE PERFETTO MODULE ext.loadline2_youtube_video;
 
 SELECT
-  loadline2_amazon_product_score() as amazon_product,
-  loadline2_cnn_article_score() as cnn_article,
-  loadline2_google_doc_score() as google_doc,
-  loadline2_google_search_result_score() as google_search_result,
-  loadline2_youtube_video_score() as youtube_video;
-
+  loadline2_amazon_product_score_visual() AS amazon_product_visual,
+  loadline2_amazon_product_score_interactive() AS amazon_product_interactive,
+  loadline2_cnn_article_score_visual() AS cnn_article_visual,
+  loadline2_cnn_article_score_interactive() AS cnn_article_interactive,
+  loadline2_google_doc_score_visual() AS google_doc_visual,
+  loadline2_google_doc_score_interactive() AS google_doc_interactive,
+  loadline2_google_search_result_score_visual() AS google_search_result_visual,
+  loadline2_google_search_result_score_interactive() AS google_search_result_interactive,
+  loadline2_youtube_video_score_visual() AS youtube_video_visual,
+  loadline2_youtube_video_score_interactive() AS youtube_video_interactive;
