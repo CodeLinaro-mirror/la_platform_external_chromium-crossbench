8078692c92f8d129d2ae6b00e3154680f175db66
Camillo Bruni <cbruni@chromium.org>
2026-05-12 02:23:39 -0700

    [tests] Use explicit android speedometer tests
    - Use separate tests to cover more flags
    - Test version 3.1 and drop v2.0
    
    Bug: 498253874
    Bypass-Check-License: already checked in parent CL
    Change-Id: Id8b74450d0302e60b25162ac2aa908c49c582bcd
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/7734948
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>

--- tests/end2end/android/speedometer/test_speedometer.py
+++ tests/end2end/android/speedometer/test_speedometer.py
@@ -3,10 +3,7 @@
 # found in the LICENSE file.
 from __future__ import annotations
 
-import enum
 from typing import TYPE_CHECKING
-
-import pytest
 
 from crossbench.cli.cli import CrossBenchCLI
 from crossbench.parse import NumberParser
@@ -16,25 +13,55 @@ if TYPE_CHECKING:
   from tests.test_helper import TestEnv
 
 
-class SpeedometerVersion(enum.StrEnum):
-  V2_0 = "2.0"
-  V2_1 = "2.1"
-  V3_0 = "3.0"
-
-
-@pytest.mark.parametrize("version", SpeedometerVersion)
-def test_speedometer(browser_config, version, test_env: TestEnv) -> None:
-  CrossBenchCLI().run([
-      f"sp{version}", f"--browser={browser_config}",
-      f"--out-dir={test_env.results_dir}", *list(test_env.cq_flags)
-  ])
-
+def _verify_metrics(test_env: TestEnv, version: str):
   with (test_env.results_dir / f"speedometer_{version}.csv").open() as csv:
     lines = csv.readlines()
     error_message = f"csv content: {lines}"
     assert "Score" in lines[-1], error_message
     assert NumberParser.positive_zero_float(
         lines[-1].split("\t")[-1]) > 0, error_message
+
+
+def test_speedometer_2_1(browser_config, test_env: TestEnv) -> None:
+  CrossBenchCLI().run([
+      "sp2.1",
+      f"--browser={browser_config}",
+      f"--out-dir={test_env.results_dir}",
+      "--iterations=1",
+      "--stories=.*Vanilla.*",
+      "--probe=performance.entries",
+  ] + list(test_env.cq_flags) + [
+      "--",
+      "--disable-field-trial-config",
+  ])
+  _verify_metrics(test_env, "2.1")
+
+
+def test_speedometer_3_0(browser_config, test_env: TestEnv) -> None:
+  CrossBenchCLI().run([
+      "sp3.0",
+      f"--browser={browser_config}",
+      f"--out-dir={test_env.results_dir}",
+      "--iterations=2",
+      "--stories=.*React.*",
+      "--probe=perfetto:default",
+      "--js-flags=--no-opt",
+  ] + list(test_env.cq_flags))
+  _verify_metrics(test_env, "3.0")
+
+
+def test_speedometer_3_1(browser_config, test_env: TestEnv) -> None:
+  CrossBenchCLI().run([
+      "speedometer_3.1",
+      f"--browser={browser_config}",
+      f"--out-dir={test_env.results_dir}",
+      "--iterations=1",
+      "--stories=.*Vue.*",
+  ] + list(test_env.cq_flags) + [
+      "--",
+      "--incognito",
+  ])
+  _verify_metrics(test_env, "3.1")
 
 
 if __name__ == "__main__":
