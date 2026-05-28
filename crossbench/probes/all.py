646081727b7f268a4ade929a701a84f1c23f61a2
Camillo Bruni <cbruni@chromium.org>
2026-05-28 00:26:54 -0700

    Implement netlog probe for chrome
    Change-Id: Id50b90189be1c2d572f2be5cd9e4379a3a1f36ce
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/7878749
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Patrick Thier <pthier@chromium.org>

--- crossbench/probes/all.py
+++ crossbench/probes/all.py
@@ -31,6 +31,7 @@ from crossbench.probes.internal.system_details import SystemDetailsProbe
 from crossbench.probes.js import JSProbe
 from crossbench.probes.json import JsonResultProbe
 from crossbench.probes.meminfo import MeminfoProbe
+from crossbench.probes.netlog import NetLogProbe
 from crossbench.probes.perfetto_info import PerfettoInfoProbe
 from crossbench.probes.performance_entries import PerformanceEntriesProbe
 from crossbench.probes.polling import PollingShellProbe
@@ -109,6 +110,7 @@ GENERAL_PURPOSE_PROBES: tuple[type[Probe], ...] = (
     JSProbe,
     LogcatAndroidProbe,
     MeminfoProbe,
+    NetLogProbe,
     PerfettoProbe,
     PerfettoInfoProbe,
     PerformanceEntriesProbe,
