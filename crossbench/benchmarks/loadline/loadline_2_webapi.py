85a3ee9314b4f0dfbf8f8bc1b43ecc4332f61f03
Victor Vianna <victorvianna@google.com>
2026-06-16 02:51:41 -0700

    Update WPR archive used by loadline2-webapi-phone to include globo fix
    Overlooked in crrev.com/c/7719645.
    
    Bug: 479819560
    Change-Id: I8d91d6174dd6d7d3f505924db007262348654f16
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/7947678
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>

--- crossbench/benchmarks/loadline/loadline_2_webapi.py
+++ crossbench/benchmarks/loadline/loadline_2_webapi.py
@@ -32,7 +32,7 @@ if TYPE_CHECKING:
 
 # We should increase the minor version number every time there are any changes
 # that might affect the benchmark score.
-VERSION_STRING: Final[str] = "2.3.0"
+VERSION_STRING: Final[str] = "2.4.0"
 
 
 class Story(StrEnum):
