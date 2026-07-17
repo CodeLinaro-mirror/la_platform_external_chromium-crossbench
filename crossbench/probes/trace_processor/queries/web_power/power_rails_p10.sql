0f87dadd72a401f260d94f7b62fb8b81aed40314
Elad Alon <eladalon@google.com>
2026-07-17 05:35:53 -0700

    Update Pixel 9/10 queries
    Change-Id: I3975c29ab6b0a091d24f07a6d80a6608b7d790c9
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/8117446
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Auto-Submit: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Igor Kraskevich <kraskevich@google.com>
    Reviewed-by: Igor Kraskevich <kraskevich@google.com>

--- crossbench/probes/trace_processor/queries/web_power/power_rails_p10.sql
+++ crossbench/probes/trace_processor/queries/web_power/power_rails_p10.sql
@@ -15,30 +15,45 @@ FROM android_power_rails_counters
 WHERE ts >= COALESCE((SELECT start_ts FROM measured_interval), (SELECT MIN(ts) FROM android_power_rails_counters))
   AND ts <= COALESCE((SELECT end_ts FROM measured_interval), (SELECT MAX(ts) FROM android_power_rails_counters))
   AND power_rail_name IN (
+    -- Main, ext
+    -- 'power.rails.wifi.bt',
+    -- 'power.VSYS_PWR_CAM_G1_uws',
+    -- 'power.VSYS_PWR_CAM_G2_uws',
+    -- 'power.VSYS_PWR_VBATT_uws',
+
+    -- Main, int
+    'power.rails.ldo.main.a',
+    'power.rails.ldo.main.b',
+    'power.rails.tpu',
+    'power.S10M_VDD_AOC_uws',
+    'power.S11M_VDD_CPU_M_uws',
+    'power.S12M_VDD_CPU1_M_uws',
     'power.S1M_VDD_AMB_uws',
     'power.S2M_VDD_CPU2_uws',
     'power.S3M_VDD_CPU1_uws',
     'power.S4M_VDD_CPU_uws',
     'power.S5M_VDD_AUR_uws',
-    'power.rails.ldo.main.a',
-    'power.rails.tpu',
-    'power.rails.ldo.main.b',
     'power.S9M_VDD_TPU_M_uws',
-    'power.S10M_VDD_AOC_uws',
-    'power.S11M_VDD_CPU_M_uws',
-    'power.S12M_VDD_CPU1_M_uws',
+
+    -- Sub, ext
+    -- 'power.rails.modem',
+    -- 'power.rails.radio.frontend',
+    -- 'power.VSYS_PWR_DISP_G1_uws',
+    -- 'power.VSYS_PWR_DISP_G2_uws',
+
+    -- Sub, int
+    'power.rails.ddr.a',
+    'power.rails.ddr.c',
+    'power.rails.ldo.sub',
+    'power.S10S_VDD_INFRA_MM_GPU_M_uws',
+    'power.S11S_UFS_VCC_uws',
+    'power.S12S_uws',
     'power.S1S_VDD_MM_uws',
     'power.S2S_VDD_GPU_uws',
-    'power.rails.ldo.sub',
-    'power.rails.ddr.c',
-    'power.rails.ddr.a',
     'power.S6S_LLDO2_uws',
     'power.S7S_MLDO_uws',
     'power.S8S_VDD_GMC_uws',
-    'power.S9S_VDD_INFRA_uws',
-    'power.S10S_VDD_INFRA_MM_GPU_M_uws',
-    'power.S11S_UFS_VCC_uws',
-    'power.S12S_uws'
+    'power.S9S_VDD_INFRA_uws'
   )
 GROUP BY 1;
 
--- crossbench/probes/trace_processor/queries/web_power/power_rails_p9.sql
+++ crossbench/probes/trace_processor/queries/web_power/power_rails_p9.sql
@@ -24,28 +24,37 @@ WHERE ts >= COALESCE(
         (SELECT end_ts FROM measured_interval),
         (SELECT MAX(ts) FROM android_power_rails_counters))
   AND power_rail_name IN (
+    -- Main, ext
+    -- 'power.rails.camera',
+    -- 'power.rails.display',
+    -- 'power.rails.radio.frontend',
+
+    -- Main, int
+    'power.rails.cpu.big',
     'power.rails.cpu.little',
     'power.rails.cpu.mid',
-    'power.rails.cpu.big',
     'power.rails.cpu.mid.mem',
-    'power.rails.gpu',
-    'power.rails.display',
-    'power.rails.multimedia',
+    'power.rails.ldo.main.a',
+    'power.rails.ldo.main.b',
     'power.rails.memory.interface',
     'power.rails.system.fabric',
+    'power.rails.tpu',
+
+    -- Sub, ext
+    -- 'power.rails.mmwave',
+    -- 'power.rails.modem',
+    -- 'power.rails.wifi.bt',
+
+    -- Sub, int
+    'power.rails.aoc.logic',
     'power.rails.ddr.a',
     'power.rails.ddr.b',
     'power.rails.ddr.c',
-    'power.rails.ldo.main.a',
-    'power.rails.ldo.main.b',
+    'power.rails.gpu',
     'power.rails.ldo.sub',
-    'power.rails.camera',
-    'power.rails.modem',
-    'power.rails.tpu',
-    'power.rails.ufs',
-    'power.rails.wifi.bt',
-    'power.rails.aoc.logic',
-    'power.S12S_VDD_AUR_uws'
+    'power.rails.multimedia',
+    'power.rails.udfps',
+    'power.rails.ufs'
   )
 GROUP BY 1;
 
