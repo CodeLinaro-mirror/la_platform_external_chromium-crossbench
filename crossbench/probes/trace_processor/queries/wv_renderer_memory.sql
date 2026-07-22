c0e10176c6bf347198957eeca0083edf859dbc9b
Mateusz Zbikowski <zbikowski@google.com>
2026-07-22 01:09:04 -0700

    Add empty renderer and browser memory metrics to wv_renderer_memory.sql.
    This change extends the query to include memory metrics (RSS anon, file,
    shmem, swap) for both the WebView renderer and browser processes after
    the only WebView has been destroyed.
    
    Change-Id: I95c326a2e246caaef89c92bb26f92eeabe47cbd7
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/8038824
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Commit-Queue: Mateusz Zbikowski <zbikowski@google.com>

--- crossbench/probes/trace_processor/queries/wv_renderer_memory.sql
+++ crossbench/probes/trace_processor/queries/wv_renderer_memory.sql
@@ -1,3 +1,4 @@
+INCLUDE PERFETTO MODULE ext.webview_startup;
 INCLUDE PERFETTO MODULE linux.memory.high_watermark;
 INCLUDE PERFETTO MODULE linux.memory.process;
 
@@ -36,6 +37,34 @@ latest_hwm AS (
   WHERE h.ts < d.ts
   ORDER BY h.ts DESC
   LIMIT 1
+),
+-- Retrieve the memory metrics for the WebView renderer process
+-- after the WebView has been destroyed.
+empty_renderer_memory AS (
+  SELECT
+    m.anon_rss AS rss_anon_empty_renderer_bytes,
+    m.file_rss AS rss_file_empty_renderer_bytes,
+    m.shmem_rss AS rss_shmem_empty_renderer_bytes,
+    m.swap AS swap_empty_renderer_bytes,
+    m.anon_rss_and_swap AS rss_anon_plus_swap_empty_renderer_bytes
+  FROM memory_rss_and_swap_per_process m
+  JOIN latest_memory lm ON m.upid = lm.upid
+  ORDER BY m.ts DESC
+  LIMIT 1
+),
+-- Retrieve the memory metrics for the WebView browser process
+-- after the WebView has been destroyed.
+empty_browser_memory AS (
+  SELECT
+    m.anon_rss AS rss_anon_empty_browser_bytes,
+    m.file_rss AS rss_file_empty_browser_bytes,
+    m.shmem_rss AS rss_shmem_empty_browser_bytes,
+    m.swap AS swap_empty_browser_bytes,
+    m.anon_rss_and_swap AS rss_anon_plus_swap_empty_browser_bytes
+  FROM memory_rss_and_swap_per_process m
+  JOIN webview_startup_start_slice AS start_slice ON m.upid = start_slice.upid
+  ORDER BY m.ts DESC
+  LIMIT 1
 )
 SELECT
   rss_anon_renderer_bytes,
@@ -43,5 +72,15 @@ SELECT
   rss_shmem_renderer_bytes,
   swap_renderer_bytes,
   rss_anon_plus_swap_renderer_bytes,
-  rss_hwm_renderer_bytes
-FROM latest_memory, latest_hwm;
+  rss_hwm_renderer_bytes,
+  rss_anon_empty_renderer_bytes,
+  rss_file_empty_renderer_bytes,
+  rss_shmem_empty_renderer_bytes,
+  swap_empty_renderer_bytes,
+  rss_anon_plus_swap_empty_renderer_bytes,
+  rss_anon_empty_browser_bytes,
+  rss_file_empty_browser_bytes,
+  rss_shmem_empty_browser_bytes,
+  swap_empty_browser_bytes,
+  rss_anon_plus_swap_empty_browser_bytes
+FROM latest_memory, latest_hwm, empty_renderer_memory, empty_browser_memory;
