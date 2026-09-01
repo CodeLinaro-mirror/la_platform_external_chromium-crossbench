f686022fa11ab86aba5251b40154e3a07a23f631
Elad Alon <eladalon@google.com>
2026-09-01 10:49:47 -0700

    Bump Web-Power version to 1.1.6
    Change-Id: I226854db38eee34c73d9b58a3114f3e168cfef60
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/8307875
    Reviewed-by: Kartar Singh <kartarsingh@google.com>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Kartar Singh <kartarsingh@google.com>

--- crossbench/benchmarks/web_power/base.py
+++ crossbench/benchmarks/web_power/base.py
@@ -47,7 +47,7 @@ if TYPE_CHECKING:
 # - The C portion of a.b.c refers to minor, score-unaffecting changes; e.g.
 #   "quality of life" improvements, changes to the cool-off period determined
 #   not to affect thermals, etc.
-VERSION_STRING: Final[str] = "1.1.5"
+VERSION_STRING: Final[str] = "1.1.6"
 
 _T = TypeVar("_T")
 StoryT = TypeVar("StoryT", bound=Story)
